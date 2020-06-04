38262bd35edce9bec3e3c468373fb01b2e24e677
Bisola <66223040+Bisola248@users.noreply.github.com>
2020-06-04 12:29:53 +0100

    Update and rename BISOLA.py to BISOLA.js

---
+++ Script/BISOLA.js
@@ -0,0 +1,1 @@
+console.log("Hello World! This is Kowode Bisola with HNG ID HNG-02218 using Javascript language and email kowodebisola@gmail.com for stage 2 task");
--- Script/BISOLA.py
+++
@@ -1,1 +0,0 @@
-print("Hello World, this is Kowode Bisola with HNGi7 ID HNG-02218 and email Kowodebisola@gmail.com using Python for stage 2 task")
